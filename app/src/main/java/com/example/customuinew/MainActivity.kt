a32123fbff68378654c48bf8e096978f9fa5f9ce
susandanne <susandanne@gmail.com>
2023-02-06 16:38:03 +0600

    vlayput object not found in main act.in main xml new custom view not showing

---
+++ app/src/main/java/com/example/customuinew/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.customuinew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+//lateinit var vsLayout:
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        vsLayout = findViewById(R.id.vsView)
+//        vsLayout.setGroup1Image(R.drawable.bd_flg)
+//        vsLayout.setGroup2Image(R.drawable.nz_flg)
+    }
+}
